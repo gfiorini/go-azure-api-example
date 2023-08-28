12f5e49f6ab817cebb48777f56a744c53476a860
Gian <gianluca.fiorinigmail.com>
2023-08-28 22:33:13 +0200

    update pipeline

--- handler.go
+++ handler.go
@@ -33,6 +33,9 @@ var albums = []Album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: "4", Title: "fdsfdsf", Artist: "aaa", Price: 39.99},
+	{ID: "5", Title: "SfsdfdsfsdfsBrown", Artist: "aaaaaaaa", Price: 39.99},
+	{ID: "6", Title: "Sardfsfdsfsdfdsfsdfdsfsdfdsfsdown", Artist: "aaaaaaaaaaaaaa", Price: 99.99},
 }
 
 func GetAlbums(c *gin.Context) {
