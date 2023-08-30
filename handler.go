d4751c1e09848c8b0c012c6425a90a1a75e6bbcd
Gian <gianluca.fiorinigmail.com>
2023-08-30 02:19:47 +0200

    test

--- handler.go
+++ handler.go
@@ -22,16 +22,18 @@ func main() {
 
 // Album represents data about a record album.
 type Album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+	ID      string  `json:"id"`
+	Title   string  `json:"title"`
+	Artist  string  `json:"artist"`
+	Price   float64 `json:"price"`
+	Summary float64 `json:"summary"`
 }
 
 // albums slice to seed record album data.
 var albums = []Album{
-	{ID: "1", Title: "RED Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "GREEN Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "1", Title: "RED Train", Artist: "John Coltrane", Price: 56.99, Summary: 1000},
+	{ID: "2", Title: "GREEN Train", Artist: "John Coltrane", Price: 56.99, Summary: 1000},
+	{ID: "3", Title: "GREEN Train", Artist: "John Coltrane", Price: 56.99, Summary: 1000},
 }
 
 func GetAlbums(c *gin.Context) {
